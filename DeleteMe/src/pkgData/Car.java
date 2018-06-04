e3f07e45a35aeaec7480fd5ce68efc84f3b09956
org <schueler@ubuntu>
2018-06-04 12:50:17 +0200

    car: get/set

--- DeleteMe/src/pkgData/Car.java
+++ DeleteMe/src/pkgData/Car.java
@@ -11,5 +11,17 @@ public class Car implements Serializable {
 
 	int id;
 	String name;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 	
 }
